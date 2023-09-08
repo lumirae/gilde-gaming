806ea79998abe5dea6a3b0682cc2fe1ab67de74f
KumaBerri <db01@ziggo.nl>
2023-09-08 11:02:26 +0200

    chatbox/cookies
    Chatbox made
    and the usernames are stored in cookies

---
+++ index.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Apen Race server running at http://localhost:${port}/`);
+});
