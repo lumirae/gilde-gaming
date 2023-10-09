3cbdf09a0f713c0e540e58643597a958ef1867e3
KumaBerri <db01@ziggo.nl>
2023-10-09 13:17:19 +0200

    qwerty

---
+++ src/server/controllers/gameController.js
@@ -0,0 +1,7 @@
+console.log("GameController.js = Loaded");
+
+
+
+module.exports = {
+
+}
---
+++ src/server/routes/gameRoute.js
@@ -0,0 +1,9 @@
+console.log('gameRoute.js = Loaded');
+
+const express = require('express');
+const router = express.Router();
+const { gameController } = require('../controllers/gameController.js');
+
+
+
+module.exports = router;
